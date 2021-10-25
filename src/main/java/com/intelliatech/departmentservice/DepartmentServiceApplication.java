6e531d9a4b0f7b3afc29a608a3afad98ca904304
adnan khan <aka034437@gmail.com>
2021-10-25 09:34:23 +0530

    changes in pox.xml file

--- src/main/java/com/intelliatech/departmentservice/DepartmentServiceApplication.java
+++ src/main/java/com/intelliatech/departmentservice/DepartmentServiceApplication.java
@@ -2,6 +2,7 @@ package com.intelliatech.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,8 +14,10 @@ public class DepartmentServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate()
 	{
+
 		return new RestTemplate();
 	}
 
